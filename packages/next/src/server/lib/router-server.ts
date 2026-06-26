74ffb4c9b471a4d4dd05055e79e5cdad72db8a37
Tim Neutkens <tim@timneutkens.nl>
2026-06-26 22:27:58 +0200

    Refine request insights traces

--- packages/next/src/server/lib/router-server.ts
+++ packages/next/src/server/lib/router-server.ts
@@ -215,7 +215,8 @@ export async function initialize(opts: {
       // reference to it.
       (req, res) => {
         return requestHandlers[opts.dir](req, res)
-      }
+      },
+      Boolean(developmentConfig.experimental.requestInsights)
     )
 
     development = {
